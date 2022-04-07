8df8df3d91e051e3cd67fa6e547e3d2e68c0a66e
Vitor <joaovitor.khanum@gmail.com>
2022-04-07 10:51:25 -0300

    Validação do código do PH

--- Program.cs
+++ Program.cs
@@ -31,7 +31,7 @@ namespace QuadrasApp
             if (res == "S" || res == "s") arquibancada = 1; else arquibancada = 0;
 
             string sql = "INSERT INTO QUADRAS (NOME, TIPO, COBERTA, BANCOS, ARQUIBANCADA, BLOQUEADA, EM_USO) "
-            + "VALUES ('" + nome + "', '" + tipo + "', '" + coberta + "', '" + bancos + "', '" + arquibancada + "', 0, 0 )";
+            + "VALUES ('" + nome + "', '" + tipo + "', '" + coberta + "', '" + bancos + "', '" + arquibancada + "', 1, 1 )";
 
             SqlConnection con = new SqlConnection(conString);
             SqlCommand cmd = new SqlCommand(sql, con);
@@ -42,10 +42,12 @@ namespace QuadrasApp
                 int i = cmd.ExecuteNonQuery();
                 if (i > 0)
                     Console.WriteLine("Quadra Cadastrada com sucesso");
-            } catch (Exception ex)
+            }
+            catch (Exception ex)
             {
                 Console.WriteLine("Erro: " + ex.Message);
-            } finally
+            }
+            finally
             {
                 con.Close();
             }
@@ -75,11 +77,11 @@ namespace QuadrasApp
                     bool data7 = reader.GetBoolean(7);
                     Console.WriteLine(data1);
                     Console.WriteLine(data2);
-                    Console.WriteLine(data3 == true ? "É Coberta" : "Nao é coberta" );
-                    Console.WriteLine(data4 == true ? "Tem bancos" : "Nao tem bancos" );
-                    Console.WriteLine(data5 == true ? "Tem arquibancada" : "Nao tem arquibancada" );
-                    Console.WriteLine(data6 == true ? "Liberada" : "Bloqueada" );
-                    Console.WriteLine(data7 == true ? "Disponivel" : "Em Uso" );
+                    Console.WriteLine(data3 == true ? "É Coberta" : "Nao é coberta");
+                    Console.WriteLine(data4 == true ? "Tem bancos" : "Nao tem bancos");
+                    Console.WriteLine(data5 == true ? "Tem arquibancada" : "Nao tem arquibancada");
+                    Console.WriteLine(data6 == true ? "Liberada" : "Bloqueada");
+                    Console.WriteLine(data7 == true ? "Disponivel" : "Em Uso");
                     Console.WriteLine("-------------#-------------");
 
                 }
@@ -93,6 +95,14 @@ namespace QuadrasApp
                 con.Close();
             }
         }
+
+        enum Menu
+        {
+            Sair = 0,
+            Cadastrar = 1,
+            Visualizar = 2
+        };
+
         static void Main(string[] args)
         {
             string conString = @"Data Source=DESKTOP-5F7JSHI\SQLEXPRESS;Initial Catalog=BDQUADRAS;Integrated Security=True";
@@ -101,17 +111,43 @@ namespace QuadrasApp
             con.Open();
             if (con.State == System.Data.ConnectionState.Open)
             {
-                Console.WriteLine("Bem Vindo!"); 
-                Console.WriteLine("Oque deseja fazer?");
-                Console.WriteLine("1- Cadastrar Quadra");
-                Console.WriteLine("2- Visualizar Quadra");
-                Console.WriteLine("");
-                string escolha = Console.ReadLine();
-                if (Convert.ToInt32(escolha) == 1)
-                    Program.CadastrarQuadra(conString);
-                else if (Convert.ToInt32(escolha) == 2)
-                    Program.VisualizarQuadras(conString);
-                
+                bool saida = false;
+                while (saida == false)
+                {
+                    Console.WriteLine("Bem Vindo!");
+                    Console.WriteLine("O que deseja fazer?\n");
+                    Console.WriteLine("1 - Cadastrar Quadra;");
+                    Console.WriteLine("2 - Visualizar Quadra;");
+
+                    Console.WriteLine("0- Sair.");
+
+                    string escolhaStr = Console.ReadLine();
+                    int escolhaInt = int.Parse(escolhaStr);
+
+                    if (escolhaInt > -1 && escolhaInt < 8)
+                    {
+                        Menu escolha = (Menu)escolhaInt;
+
+                        switch (escolha)
+                        {
+                            case Menu.Sair:
+                                saida = true;
+                                break;
+                            case Menu.Cadastrar:
+                                Program.CadastrarQuadra(conString);
+                                break;
+                            case Menu.Visualizar:
+                                Program.VisualizarQuadras(conString);
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        saida = true;
+                    }
+                    Console.ReadLine();
+                    Console.Clear();
+                }
             }
         }
     }
